0517bd5a3d416edb5bd1ebf51452b5bff99e0bd0
porthole-ascend-cinnamon <porthole-ascend-cinnamon@protonmail.com>
2022-04-28 19:29:40 +0300

    updates

--- finder.py
+++ finder.py
@@ -1,23 +1,56 @@
 # @formatter:off
-try:import gevent.monkey; gevent.monkey.patch_all()
-except:raise
+try: import colorama; colorama.init()
+except: pass
+try:
+    import gevent.monkey; gevent.monkey.patch_all()
+    GEVENT = True
+except ImportError:
+    GEVENT = False
 # @formatter:on
 import argparse
+import itertools
 import os
 import random
+import threading
 import time
 from ipaddress import IPv4Address
 from threading import Event, Thread
 
 from PyRoxy import Proxy, ProxyType, Tools
+from colorama import Fore
 
 from core import JUDGES, fix_ulimits, logger
 from networks import random_ip_range
 from report import report_proxy
 
 
-CHECKED = 0
-FOUND = 0
+class cl:
+    MAGENTA = Fore.LIGHTMAGENTA_EX
+    BLUE = Fore.LIGHTBLUE_EX
+    GREEN = Fore.LIGHTGREEN_EX
+    YELLOW = Fore.LIGHTYELLOW_EX
+    RED = Fore.LIGHTRED_EX
+    RESET = Fore.RESET
+
+
+class FastWriteCounter:
+    def __init__(self):
+        self._number_of_read = 0
+        self._counter = itertools.count()
+        self._read_lock = threading.Lock()
+
+    def increment(self):
+        next(self._counter)
+
+    def value(self):
+        with self._read_lock:
+            value = next(self._counter) - self._number_of_read
+            self._number_of_read += 1
+        return value
+
+
+CHECKED = FastWriteCounter()
+FOUND = FastWriteCounter()
 
 PORTS = [
     (8080, ProxyType.HTTP),
@@ -28,9 +61,11 @@ PORTS = [
 def _report_proxy(proxy):
     result = report_proxy(proxy)
     if result:
-        logger.info(f'Проксі {proxy} надіслано, дякуємо!')
+        logger.info(f'{cl.GREEN}Проксі {proxy} надіслано, дякуємо!{cl.RESET}')
     else:
-        logger.warning(f'На жаль, проксі {proxy} не надіслане - зверніться до адміністратора')
+        logger.warning(
+            f'{cl.RED}На жаль, проксі {proxy} не надіслане - зверніться до адміністратора @ddosseparbot{cl.RESET}'
+        )
 
 
 def report_success(proxy):
@@ -48,7 +83,6 @@ def generate_ip() -> str:
 
 
 def _try_host(event, out, host, timeout, retries):
-    global CHECKED, FOUND
     try:
         for judge in random.sample(JUDGES, retries):
             for port, proto in PORTS:
@@ -56,9 +90,9 @@ def _try_host(event, out, host, timeout, retries):
                     return
 
                 proxy = Proxy(host, port, proto)
-                CHECKED += 1
+                CHECKED.increment()
                 if proxy.check(judge, timeout):
-                    FOUND += 1
+                    FOUND.increment()
                     report_success(proxy)
                     out.write(str(proxy) + '\n')
                     return
@@ -83,7 +117,7 @@ def main(file):
     fix_ulimits()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--threads', type=int, default=5000)
+    parser.add_argument('--threads', type=int, default=5000 if GEVENT else 2000)
     parser.add_argument('--timeout', type=int, default=3)
     parser.add_argument('--retries', type=int, default=1)
     parser.add_argument('--socks5', default=False, action='store_true')
@@ -97,7 +131,10 @@ def main(file):
         ))
 
     threads = args.threads
-    threads_limit = 10000
+    threads_limit = 10000 if GEVENT else 5000
+    if not GEVENT:
+        logger.warning(f'{cl.MAGENTA}gevent не встановлено - потребується більше системних ресурсів{cl.RESET}')
+
     if threads > threads_limit:
         logger.warning(f'Обмеження {threads_limit} потоків!')
         threads = threads_limit
@@ -109,11 +146,11 @@ def main(file):
         event = Event()
         event.set()
         start_workers(threads, event, file, args.timeout, args.retries)
-        logger.info('Усі процеси запущено!')
+        logger.info(f'{cl.BLUE}Усі процеси запущено!{cl.RESET}')
 
         while event.is_set():
             time.sleep(period)
-            logger.info(f'Перевірено: {CHECKED} | Знайдено: {FOUND}')
+            logger.info(f'Перевірено: {CHECKED.value()} | Знайдено: {FOUND.value()}')
             file.flush()
             iterations += 1
             if period * iterations > restart_after:
@@ -125,17 +162,20 @@ def main(file):
 
 def main_wrapper():
     filename = f'proxy_{int(time.time())}.txt'
-    logger.info(f'Проксі будуть автоматично відправлені на сервер, а також збережені у файл {filename}')
+    logger.info(
+        f'Проксі будуть автоматично відправлені на сервер, а також збережені у файл {cl.YELLOW}{filename}{cl.RESET}'
+    )
     file = open(filename, 'w')
     try:
         main(file)
     except:
-        logger.info('Завершуємо роботу')
+        logger.info(f'{Fore.LIGHTBLUE_EX}Завершуємо роботу{Fore.RESET}')
     finally:
-        if FOUND:
-            logger.info(f'Збережено {FOUND} у файл {filename}')
+        found = FOUND.value()
+        if found:
+            logger.info(f'Збережено {cl.BLUE}{found}{cl.RESET} у файл {cl.YELLOW}{filename}{cl.RESET}')
         else:
-            logger.warning(f'Проксі не знайдено, видаляємо файл {filename}')
+            logger.warning(f'Проксі не знайдено, видаляємо файл {cl.YELLOW}{filename}{cl.RESET}')
             os.remove(filename)
         file.close()
 
