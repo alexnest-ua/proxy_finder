a9f6d18a682ed92c6d93f9acc023517a4e4e3407
porthole-ascend-cinnamon <porthole-ascend-cinnamon@protonmail.com>
2022-05-03 22:14:03 +0300

    fix

--- httpparser.py
+++ httpparser.py
@@ -7,16 +7,195 @@ import os
 import re
 import sys
 
-if sys.version_info >= (3,):
-    import urllib.parse as urlparse
-else:
-    import urlparse
+import urllib.parse as urlparse
 
 import zlib
+from collections.abc import MutableMapping
 
-from http_parser.util import (b, bytes_to_str, IOrderedDict, StringIO,
-        unquote, MAXSIZE)
+from urllib.parse import unquote
 
+
+class IOrderedDict(dict, MutableMapping):
+    'Dictionary that remembers insertion order with insensitive key'
+
+    # An inherited dict maps keys to values.
+    # The inherited dict provides __getitem__, __len__, __contains__, and get.
+    # The remaining methods are order-aware.
+    # Big-O running times for all methods are the same as for regular dictionaries.
+
+    # The internal self.__map dictionary maps keys to links in a doubly linked list.
+    # The circular doubly linked list starts and ends with a sentinel element.
+    # The sentinel element never gets deleted (this simplifies the algorithm).
+    # Each link is stored as a list of length three:  [PREV, NEXT, KEY].
+
+    def __init__(self, *args, **kwds):
+        '''Initialize an ordered dictionary.  Signature is the same as for
+        regular dictionaries, but keyword arguments are not recommended
+        because their insertion order is arbitrary.
+
+        '''
+        if len(args) > 1:
+            raise TypeError('expected at most 1 arguments, got %d' % len(args))
+        try:
+            self.__root
+        except AttributeError:
+            self.__root = root = [None, None, None]  # sentinel node
+            PREV = 0
+            NEXT = 1
+            root[PREV] = root[NEXT] = root
+            self.__map = {}
+        self.__lower = {}
+        self.update(*args, **kwds)
+
+    def __setitem__(self, key, value, PREV=0, NEXT=1, dict_setitem=dict.__setitem__):
+        'od.__setitem__(i, y) <==> od[i]=y'
+        # Setting a new item creates a new link which goes at the end of the linked
+        # list, and the inherited dictionary is updated with the new key/value pair.
+        if key not in self:
+            root = self.__root
+            last = root[PREV]
+            last[NEXT] = root[PREV] = self.__map[key] = [last, root, key]
+            self.__lower[key.lower()] = key
+        key = self.__lower[key.lower()]
+        dict_setitem(self, key, value)
+
+    def __delitem__(self, key, PREV=0, NEXT=1, dict_delitem=dict.__delitem__):
+        'od.__delitem__(y) <==> del od[y]'
+        # Deleting an existing item uses self.__map to find the link which is
+        # then removed by updating the links in the predecessor and successor nodes.
+        if key in self:
+            key = self.__lower.pop(key.lower())
+
+        dict_delitem(self, key)
+        link = self.__map.pop(key)
+        link_prev = link[PREV]
+        link_next = link[NEXT]
+        link_prev[NEXT] = link_next
+        link_next[PREV] = link_prev
+
+    def __getitem__(self, key, dict_getitem=dict.__getitem__):
+        if key in self:
+            key = self.__lower.get(key.lower())
+        return dict_getitem(self, key)
+
+    def __contains__(self, key):
+        return key.lower() in self.__lower
+
+    def __iter__(self, NEXT=1, KEY=2):
+        'od.__iter__() <==> iter(od)'
+        # Traverse the linked list in order.
+        root = self.__root
+        curr = root[NEXT]
+        while curr is not root:
+            yield curr[KEY]
+            curr = curr[NEXT]
+
+    def __reversed__(self, PREV=0, KEY=2):
+        'od.__reversed__() <==> reversed(od)'
+        # Traverse the linked list in reverse order.
+        root = self.__root
+        curr = root[PREV]
+        while curr is not root:
+            yield curr[KEY]
+            curr = curr[PREV]
+
+    def __reduce__(self):
+        'Return state information for pickling'
+        items = [[k, self[k]] for k in self]
+        tmp = self.__map, self.__root
+        del self.__map, self.__root
+        inst_dict = vars(self).copy()
+        self.__map, self.__root = tmp
+        if inst_dict:
+            return (self.__class__, (items,), inst_dict)
+        return self.__class__, (items,)
+
+    def clear(self):
+        'od.clear() -> None.  Remove all items from od.'
+        try:
+            for node in self.__map.values():
+                del node[:]
+            self.__root[:] = [self.__root, self.__root, None]
+            self.__map.clear()
+        except AttributeError:
+            pass
+        dict.clear(self)
+
+    def get(self, key, default=None):
+        if key in self:
+            return self[key]
+        return default
+
+    setdefault = MutableMapping.setdefault
+    update = MutableMapping.update
+    pop = MutableMapping.pop
+    keys = MutableMapping.keys
+    values = MutableMapping.values
+    items = MutableMapping.items
+    __ne__ = MutableMapping.__ne__
+
+    def popitem(self, last=True):
+        '''od.popitem() -> (k, v), return and remove a (key, value) pair.
+        Pairs are returned in LIFO order if last is true or FIFO order if false.
+
+        '''
+        if not self:
+            raise KeyError('dictionary is empty')
+        key = next(reversed(self) if last else iter(self))
+        value = self.pop(key)
+        return key, value
+
+    def __repr__(self):
+        'od.__repr__() <==> repr(od)'
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, list(self.items()))
+
+    def copy(self):
+        'od.copy() -> a shallow copy of od'
+        return self.__class__(self)
+
+    @classmethod
+    def fromkeys(cls, iterable, value=None):
+        '''OD.fromkeys(S[, v]) -> New ordered dictionary with keys from S
+        and values equal to v (which defaults to None).
+
+        '''
+        d = cls()
+        for key in iterable:
+            d[key] = value
+        return d
+
+    def __eq__(self, other):
+        '''od.__eq__(y) <==> od==y.  Comparison to another OD is order-sensitive
+        while comparison to a regular mapping is order-insensitive.
+
+        '''
+        if isinstance(other, IOrderedDict):
+            return len(self) == len(other) and \
+                   set(self.items()) == set(other.items())
+        return dict.__eq__(self, other)
+
+    def __del__(self):
+        self.clear()  # eliminate cyclical references
+
+
+def b(s):
+    return s.encode("latin-1")
+
+
+def bytes_to_str(b):
+    return str(b, 'latin1')
+
+
+string_types = str,
+
+import io
+
+
+StringIO = io.StringIO
+
+MAXSIZE = sys.maxsize
 
 METHOD_RE = re.compile("[A-Z0-9$-_.]{3,20}")
 VERSION_RE = re.compile("HTTP/(\d+).(\d+)")
@@ -28,14 +207,18 @@ BAD_FIRST_LINE = 0
 INVALID_HEADER = 1
 INVALID_CHUNK = 2
 
+
 class InvalidRequestLine(Exception):
     """ error raised when first line is invalid """
+
 
 class InvalidHeader(Exception):
     """ error raised on invalid header """
 
+
 class InvalidChunkSize(Exception):
     """ error raised when we parse an invalid chunk size """
+
 
 class HttpParser(object):
 
@@ -57,7 +240,7 @@ class HttpParser(object):
         self._url = None
         self._path = None
         self._query_string = None
-        self._fragment= None
+        self._fragment = None
         self._headers = IOrderedDict()
         self._environ = dict()
         self._chunked = False
@@ -112,7 +295,7 @@ class HttpParser(object):
                 environ[key] = environ.pop(hkey)
 
         script_name = environ.get('SCRIPT_NAME',
-                os.environ.get("SCRIPT_NAME", ""))
+                                  os.environ.get("SCRIPT_NAME", ""))
         if script_name:
             path_info = self._path.split(script_name, 1)[1]
             environ.update({
@@ -210,7 +393,7 @@ class HttpParser(object):
                     first_line = bytes_to_str(b("").join(self._buf))
                     nb_parsed = nb_parsed + idx + 2
 
-                    rest = data[idx+2:]
+                    rest = data[idx + 2:]
                     data = b("")
                     if self._parse_firstline(first_line):
                         self._buf = [rest]
@@ -256,7 +439,7 @@ class HttpParser(object):
 
     def _parse_firstline(self, line):
         try:
-            if self.kind == 2: # auto detect
+            if self.kind == 2:  # auto detect
                 try:
                     self._parse_request_line(line)
                 except InvalidRequestLine:
@@ -315,7 +498,7 @@ class HttpParser(object):
         self._version = (int(match.group(1)), int(match.group(2)))
 
         # update environ
-        if hasattr(self,'_environ'):
+        if hasattr(self, '_environ'):
             self._environ.update({
                 "PATH_INFO": self._path,
                 "QUERY_STRING": self._query_string,
@@ -325,7 +508,7 @@ class HttpParser(object):
 
     def _parse_headers(self, data):
         idx = data.find(b("\r\n\r\n"))
-        if idx < 0: # we don't have all headers
+        if idx < 0:  # we don't have all headers
             if self._status_code == 204 and data == b("\r\n"):
                 self._buf = []
                 self.__on_headers_complete = True
@@ -335,7 +518,7 @@ class HttpParser(object):
 
         # Split lines on \r\n keeping the \r\n on each line
         lines = [bytes_to_str(line) + "\r\n" for line in
-                data[:idx].split(b("\r\n"))]
+                 data[:idx].split(b("\r\n"))]
 
         # Parse headers into key/value pairs paying attention
         # to continuation lines.
@@ -370,7 +553,7 @@ class HttpParser(object):
             self._headers[name] = value
 
             # update WSGI environ
-            key =  'HTTP_%s' % name.upper().replace('-','_')
+            key = 'HTTP_%s' % name.upper().replace('-', '_')
             self._environ[key] = value
 
         # detect now if body is sent by chunks.
@@ -391,12 +574,12 @@ class HttpParser(object):
         encoding = self._headers.get('content-encoding')
         if self.decompress:
             if encoding == "gzip":
-                self.__decompress_obj = zlib.decompressobj(16+zlib.MAX_WBITS)
+                self.__decompress_obj = zlib.decompressobj(16 + zlib.MAX_WBITS)
                 self.__decompress_first_try = False
             elif encoding == "deflate":
                 self.__decompress_obj = zlib.decompressobj()
 
-        rest = data[idx+4:]
+        rest = data[idx + 4:]
         self._buf = [rest]
         self.__on_headers_complete = True
         return len(rest)
@@ -421,7 +604,6 @@ class HttpParser(object):
                         body_part = self.__decompress_obj.decompress(body_part)
                     self.__decompress_first_try = False
 
-
             self._partial_body = True
             self._body.append(body_part)
             self._buf = []
@@ -445,7 +627,6 @@ class HttpParser(object):
             if size is None or len(rest) < size + 2:  # wait till terminator
                 return None
 
-
             body_part, rest = rest[:size], rest[size:]
             if len(rest) < 2:
                 self.errno = INVALID_CHUNK
@@ -466,7 +647,7 @@ class HttpParser(object):
         idx = data.find(b("\r\n"))
         if idx < 0:
             return None, None
-        line, rest_chunk = data[:idx], data[idx+2:]
+        line, rest_chunk = data[:idx], data[idx + 2:]
         chunk_size = line.split(b(";"), 1)[0].strip()
         try:
             chunk_size = int(chunk_size, 16)
