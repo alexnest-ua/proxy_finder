8dc6e2c82fe9b9b47c09b7e657f0dc44ecc43b4a
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-08 01:25:22 +0300

    Update auto_runner_linux.sh

--- auto_runner_linux.sh
+++ auto_runner_linux.sh
@@ -45,17 +45,16 @@ sleep 4s
 # Restarts attacks and update targets list every 20 minutes
 while [ 1 == 1 ]
 do	
-	cd ~/proxy_finder
+  cd ~/proxy_finder
 	
-
-	num=$(sudo git pull origin main | grep -P -c 'Already|Уже')
+  num=$(sudo git pull origin main | grep -P -c 'Already|Уже')
   echo "$num"
    	
   if ((num == 1));
   then	
-		echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running up to date proxy_finder"
+		echo -e "\n\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running up to date proxy_finder\n\n"
 	else
-		echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running updated proxy_finder"
+		echo -e "\n\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running updated proxy_finder\n\n"
 		bash auto_runner_linux.sh $threads& # run new downloaded script 
 		return 0 #terminate old script
 	fi
