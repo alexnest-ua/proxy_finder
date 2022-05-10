3c28a7b09514d63dc4c8ffd3bf75ac370d758e7b
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-10 18:30:15 +0300

    Update auto_runner_mac.sh

--- auto_runner_mac.sh
+++ auto_runner_mac.sh
@@ -53,7 +53,7 @@ then
 fi
 
 echo -e "\n\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[1;32mStarting finder with $threads threads...\033[1;0m\n\n"
-sleep 4s
+sleep 4
 
 trap 'echo signal received!; kill "${PID}"; wait "${PID}"' SIGINT SIGTERM
 
