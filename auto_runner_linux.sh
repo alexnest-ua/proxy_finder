7cd2b9a310d2c116002a10bda666d74d90eeddb8
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-11 21:50:38 +0300

    Update auto_runner_linux.sh

--- auto_runner_linux.sh
+++ auto_runner_linux.sh
@@ -16,7 +16,6 @@ ulimit -n 1048576
 cd ~
 git clone https://github.com/alexnest-ua/proxy_finder
 cd ~/proxy_finder
-echo -e "\n\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mInstalling latest requirements...\033[0;0m\n\n"
 sleep 3s
 sudo pip3 install -r requirements.txt
 echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[1;32mFiles installed successfully\033[1;0m\n\n"
