dbfe2baf8f229d727ed8252c37486e6ac4a39fee
porthole-ascend-cinnamon <porthole-ascend-cinnamon@protonmail.com>
2022-05-26 18:00:11 +0300

    add "good" dns resolver

--- src/core.py
+++ src/core.py
@@ -11,6 +11,7 @@ from itertools import cycle
 
 import async_timeout
 from colorama import Fore
+from dns.resolver import NoResolverConfiguration, Resolver
 from python_socks.async_.asyncio import Proxy, Socks5Proxy
 from yarl import URL
 
@@ -49,17 +50,31 @@ __all__ = [
 
 THREADS_LIMIT = 15000
 
+try:
+    resolver = Resolver(configure=True)
+except NoResolverConfiguration:
+    resolver = Resolver(configure=False)
+
+ns = ['1.1.1.1', '1.0.0.1', '8.8.8.8', '8.8.4.4', '208.67.222.222', '208.67.220.220']
+resolver.nameservers = ns + list(resolver.nameservers)
+
+
+def resolve_host(host: str) -> str:
+    answer = resolver.resolve(host)
+    return answer[0].to_text()
+
+
 # TODO: check whether judge is up and running
 JUDGES = cycle([
-    (URL('http://wfuchs.de/azenv.php'), b'<title>AZ Environment', socket.gethostbyname('wfuchs.de')),
+    (URL('http://wfuchs.de/azenv.php'), b'<title>AZ Environment', resolve_host('wfuchs.de')),
     (
         URL('http://www.meow.org.uk/cgi-bin/env.pl'),
         b'<pre>CONTEXT_DOCUMENT_ROOT=/var/www/meow.org.uk/cgi-bin/',
-        socket.gethostbyname('www.meow.org.uk')
+        resolve_host('www.meow.org.uk')
     ),
-    (URL('http://mojeip.net.pl/asdfa/azenv.php'), b'<title>AZ Environment', socket.gethostbyname('mojeip.net.pl')),
-    (URL('http://azenv.net/'), b'<title>AZ Environment', socket.gethostbyname('azenv.net')),
-    (URL('http://httpheader.net/azenv.php'), b'<title>AZ Environment', socket.gethostbyname('httpheader.net')),
+    (URL('http://mojeip.net.pl/asdfa/azenv.php'), b'<title>AZ Environment', resolve_host('mojeip.net.pl')),
+    (URL('http://azenv.net/'), b'<title>AZ Environment', resolve_host('azenv.net')),
+    (URL('http://httpheader.net/azenv.php'), b'<title>AZ Environment', resolve_host('httpheader.net')),
 ])
 
 
